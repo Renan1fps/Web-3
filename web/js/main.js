14c3de840bffe71431a852d46460e9af13acab9a
Renan1fps <renan.sergipe68@gmail.com>
2023-12-10 15:31:38 -0300

    refactor: add like button

--- web/js/main.js
+++ web/js/main.js
@@ -2,19 +2,19 @@ window.addEventListener("load", verifyLogin);
 
 function verifyLogin() {
   const token = localStorage.getItem("@token");
-  const cadUser = document.getElementById('create-user');
-  const cadAnime = document.getElementById('create-anime');
- 
+  const cadUser = document.getElementById("create-user");
+  const cadAnime = document.getElementById("create-anime");
+
   if (!token) {
     window.location.href = "index.html";
   }
 
-  const user = JSON.parse(localStorage.getItem("@user"))
+  const user = JSON.parse(localStorage.getItem("@user"));
 
-  if(user.profileType === 2){
-    cadUser.style.display = 'none';
-    cadAnime.style.display = 'none';
-    deleteButton.style.display = 'none'
+  if (user.profileType === 2) {
+    cadUser.style.display = "none";
+    cadAnime.style.display = "none";
+    deleteButton.style.display = "none";
   }
 }
 
@@ -57,8 +57,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const likeButton = document.createElement("button");
     likeButton.classList.add("like-button");
-    likeButton.textContent = "Like";
 
+    likeButton.addEventListener('click', ()=> {
+      likeButton.style.backgroundColor = 'red'
+    })
+    
+    const icon = document.createElement("img");
+    icon.src = "./public/coracao.png";
+    icon.style.maxHeight = '1rem'
+    icon.style.maxHeight = '1rem'
+    likeButton.appendChild(icon);
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("like-button");
@@ -66,22 +74,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const user = JSON.parse(localStorage.getItem("@user"));
 
-    if(user.profileType === 2){
-      deleteButton.style.display = 'none'
+    if (user.profileType === 2) {
+      deleteButton.style.display = "none";
     }
 
     deleteButton.addEventListener("click", async function () {
       await fetch(`http://localhost:8000/animes/${anime.id}`, {
-         method: "DELETE",
-         headers: { 'Content-Type': 'application/json' },
-       });
-       window.location.href = "main.html";
-     });
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      window.location.href = "main.html";
+    });
 
     likeButton.addEventListener("click", async function () {
-     await fetch(`http://localhost:8000/animes/${anime.id}`, {
+      await fetch(`http://localhost:8000/animes/${anime.id}`, {
         method: "PUT",
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ popularity: 1 }),
       });
     });
@@ -105,11 +113,29 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const faleConoscoBtn = document.getElementById("faleConoscoBtn");
+  const faleConoscoBtnModal = document.getElementById("faleConoscoBtn");
+
+  const faleConoscoBtn = document.getElementById("enviarBtn");
+
+  faleConoscoBtn.addEventListener("click", async () => {
+    const faleConosco = document.getElementById("descricao-fale").value;
+
+    const user = JSON.parse(localStorage.getItem("@user"));
+
+    await fetch(`http://localhost:8000/users/${user.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ faleConosco: faleConosco }),
+    });
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+    alert("Sugestão cadastrada!");
+  });
+
   const modal = document.getElementById("modal");
   const closeModalBtn = document.getElementById("closeModal");
 
-  faleConoscoBtn.addEventListener("click", function () {
+  faleConoscoBtnModal.addEventListener("click", function () {
     modal.style.display = "flex";
   });
 
@@ -124,12 +150,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const profileCircle = document.getElementById('profileCircle');
-    const profileTooltip = document.getElementById('profileTooltip');
+document.addEventListener("DOMContentLoaded", function () {
+  const profileCircle = document.getElementById("profileCircle");
+  const profileTooltip = document.getElementById("profileTooltip");
 
-    profileCircle.addEventListener('click', function () {
-        profileTooltip.style.display = (profileTooltip.style.display === 'block') ? 'none' : 'block';
-    });
+  profileCircle.addEventListener("click", function () {
+    profileTooltip.style.display =
+      profileTooltip.style.display === "block" ? "none" : "block";
+  });
 });
-
